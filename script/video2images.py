dbeb92da8a161891ac172a2ac1e162006f0a9cfe
vivianbh <hadallir@gmail.com>
2024-08-01 16:10:30 +0800

    extract each 0.5 sec

--- script/video2images.py
+++ script/video2images.py
@@ -3,16 +3,20 @@ import os
 
 cam = cv2.VideoCapture("video/output.mp4")
 
-frameno = 1
+frame_rate = 2  # Desired frame rate (1 frame every 0.5 seconds)
+frame_count = 1
+
 while(True):
    ret,frame = cam.read()
    if ret:
       # if video is still left continue creating images
-      name = 'images/' + str(frameno) + '.jpg'
-      print ('new frame captured...' + name)
+      name = 'images/' + str(frame_count) + '.jpg'
 
-      cv2.imwrite(name, frame)
-      frameno += 1
+      if frame_count % int(cam.get(5)/frame_rate) == 0:
+         cv2.imwrite(name, frame)
+         print ('new frame captured...' + name)
+      
+      frame_count += 1
    else:
       break
 
